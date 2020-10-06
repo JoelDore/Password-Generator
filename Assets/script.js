7f6345774b00f02a08280b5fdb48ebad04acba46
JES20 <dore.joel.dore@gmail.com>
2020-10-06 12:12:27 -0400

    Created objects containing character types and user prompts

--- Assets/script.js
+++ Assets/script.js
@@ -12,7 +12,18 @@ function writePassword() {
 // Function to generate password string
 function generatePassword() {
   // Create objects with arrays of possible characters types and prompt messages
-
+  var charTypes = {
+    lc: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    uc: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    symbols: ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "\\", ']', '^', '_', '`', '{', '|', '}', '~']
+  };
+  var prompts = {
+    lc: ['Should the password contain lowercase letters?'],
+    uc: ['Should the password contain uppercase letters?'],
+    digits: ['Should the password contain numbers?'],
+    symbols: ['Should the password contain special characters?']
+  };
   // Prompt user for length of password
 
   // Validate length is within bounds
